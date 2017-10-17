4973450a357bf73d952b7c9080cca943670b9ee5
Joshua Liu <liuchaozhenyu@gmail.com>
2017-10-17 17:54:05 +0800

    add acf pacf

--- algorithm/arima.py
+++ algorithm/arima.py
@@ -4,8 +4,85 @@
 # @Author: Joshua Liu
 # @Email: liuchaozhenyu@gmail.com
 # @Create Date: 2017-10-16 17:10:33
-# @Last Modified: 2017-10-16 18:10:33
+# @Last Modified: 2017-10-17 17:10:06
 # @Description:
+#   ARIMA(p, d, q) 模型
+#       d: 差分次数，使用"ADF单位根平稳型检测"，得到合适的 d 
+#       p, q: 使用自相关图(acf)和偏自相关图(pacf)，选择 p, q 值
+#          赤池信息量:   AIC = -2 ln(L) + 2 k
+#          贝叶斯信息量: BIC = -2 ln(L) + ln(n) * k
+#          汉南奎恩标准: HQ = -2 ln(L) + ln(ln(n)) * k
+#   ARIMA 模型检验
+#       - 自相关图和偏自相关图
+#       - D-W 检验
+#       - Ljung-Box 检验
+#   ARIMA 模型预测
+
 import pandas as pd
 import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.stattools import acf, pacf
 
+# lambda 函数，解析时间
+dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m')
+
+class ArimaModel():
+
+    def __init__(self, filepath):
+        self.data = pd.read_csv(filepath, parse_dates=['Month'], index_col='Month', date_parser=dateparse)
+        self.ts = self.data['#Passengers']
+        self.api_dict = {
+            'acf': self.acf,
+            'pacf': self.pacf,
+        }
+
+
+    def callback(self, api):
+        return self.api_dict[api]()
+
+    def acf(self):
+        return acf(self.ts, nlags=20)
+
+
+    def pacf(self):
+        return pacf(self.ts, nlags=20)
+
+
+
+# data = pd.read_csv('./data/AirPassengers.csv', parse_dates=['Month'],
+#         index_col='Month', date_parser=dateparse)
+# ts = data['#Passengers']
+# 
+# ts_log = np.log(ts)
+# moving_avg = pd.Series.rolling(ts_log, 12).mean()
+# ts_log_moving_avg_diff = ts_log - moving_avg
+# expwighted_avg = pd.ewma(ts_log, halflife=12)
+# ts_log_ewma_diff = ts_log - expwighted_avg
+# ts_log_diff = ts_log - ts_log.shift()
+# 
+# 
+# # Decomposition
+# decomposition = seasonal_decompose(ts_log)
+# 
+# # ACF & PACF Plots
+# lag_acf = acf(ts_log_diff, nlags=20)
+# lag_pacf = pacf(ts_log_diff, nlags=20, method='ols')
+# 
+# # AR Model
+# ar_model = ARIMA(ts_log, order=(2, 1, 0))
+# results_AR = ar_model.fit(disp=-1)
+# 
+# # MA Model
+# ma_model = ARIMA(ts_log, order=(0, 1, 2))
+# results_MA = ma_model.fit(disp=-1)
+# 
+# # ARIMA Model
+# arima_model = ARIMA(ts_log, order=(2, 1, 2))
+# results_ARIMA = arima_model.fit(disp=-1)
+# 
+# # Convert to original scale
+# predictions_ARIMA_diff = pd.Series(results_ARIMA.fittedvalues, copy=True)
+# predictions_ARIMA_log = pd.Series(ts_log.ix[0], index=ts_log.index)
+# predictions_ARIMA_log = predictions_ARIMA_log.add(predictions_ARIMA_diff.cumsum(), fill_value=0)
+# predictions_ARIMA = np.exp(predictions_ARIMA_log)
--- tspredict.py
+++ tspredict.py
@@ -4,49 +4,128 @@
 # @Author: Joshua Liu
 # @Email: liuchaozhenyu@gmail.com
 # @Create Date: 2017-10-16 16:10:57
-# @Last Modified: 2017-10-16 17:10:55
+# @Last Modified: 2017-10-17 17:10:28
 # @Description:
 #   flask 框架实现的 Restful API 接口
 
 from flask import Flask
-from flask.ext.restful import Resource
-from flask.ext.restful import Api
-from flask.ext.restful import reqparse
-from flask.ext.restful import abort
+from flask_restful import Resource
+from flask_restful import Api
+from flask_restful import reqparse
+from flask_restful.reqparse import Argument
+from flask_restful import abort
+from werkzeug.datastructures import FileStorage
+from io import StringIO
+import uuid
+import csv
+import sys
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('data', type=str)
+ddir = './data/'
+if not os.path.isdir(ddir):
+    os.mkdir(ddir)
 
 class Root(Resource):
     '''
     根，目前返回使用说明
     '''
     def get(self):
-        return {'tspredict': 'OK'}
+        return 'api 说明：\n/data/: 上传数据\narima/<id> 请求 arima 算法'
 
 
 api.add_resource(Root, '/')
 
 
-class Prediction(Resource):
+class FileStorageArgument(Argument):
     '''
-    第三方接口封装
+    用于 flask-restful 接受所有的上传文件
     '''
-    def get(self):
-        return {'prediction': 'foo'}
+    def convert(self, value, op):
+        if self.type is FileStorage:
+            return value
+        super(FileStorageArgument, self).convert(**args, **kwargs)
+
+#
+# DataApi
+#
+data_dict = {}
+for fl in os.listdir(ddir):
+    if fl.endswith('.csv'):
+        data_dict[fl[:-4]] = ddir + fl
+
+class DataApi(Resource):
+    '''
+    数据接口
+    '''
+    def __init__(self):
+        self.parser = reqparse.RequestParser(argument_class=FileStorageArgument)
+        self.parser.add_argument('file', required=True, type=FileStorage, help='csv file', location='files')
+        super(DataApi, self).__init__()
+
+    def put(self):
+        args = self.parser.parse_args()
+        csvfile = args['file']
+
+        # TODO: 文件检查
+
+        fio = StringIO()
+        csvfile.save(fio)
+        return fio
+
 
     def post(self):
-        args = parser.parse_args()
-        data = args['data']
-        return data
+        args = self.parser.parse_args()
+        csvfile = args['file']
+        fid = self.genid()
+        with open(ddir + fid + '.csv', 'wb') as f:
+            csvfile.save(f)
+        data_dict[fid] = ddir + fid + '.csv'
+        return {'id': fid}
+
+    def genid(self):
+        newid = str(uuid.uuid4())[:8]
+        ids = data_dict.keys()
+        while newid in ids:
+            newid = str(uuid.uuid4())[:8]
+        return newid
+
+
+api.add_resource(DataApi, '/data/')
+
+
+from algorithm.arima import ArimaModel
+
+class ArimaApi(Resource):
+    '''
+    '''
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('api', required=True, type=str)
+        super(ArimaApi, self).__init__()
+
+
+    def get(self, fid):
+        self.abort_if_id_not_exist(fid)
+        args = self.parser.parse_args()
+        api = args['api']
+        arima = ArimaModel(data_dict[fid])
+        return {api: arima.callback(api).tolist()}
+
+    def post(self):
+        pass
 
     def put(self):
         pass
 
-api.add_resource(Prediction, '/api/')
+
+    def abort_if_id_not_exist(self, fid):
+        if fid not in data_dict.keys():
+            abort(404, message="{} is not exist".format(fid))
+
+api.add_resource(ArimaApi, '/arima/<string:fid>')
 
 if __name__ == '__main__':
     app.run(debug=True)
